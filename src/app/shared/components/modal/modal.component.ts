65d0b843fdc081ff68f7e22d58e7ebd4f43df96c
Nelson <punkpunkada@gmail.com>
2023-03-10 10:28:58 -0300

    minimizando depedencias

--- src/app/shared/components/modal/modal.component.ts
+++ src/app/shared/components/modal/modal.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ModalConfig } from './interfaces/modal-config';
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModalComponent implements OnInit {
 
+  public config: ModalConfig;
+  
   constructor() { }
 
   ngOnInit(): void {
   }
+
 
 }
--- src/app/shared/components/modal/services/modal.service.ts
+++ src/app/shared/components/modal/services/modal.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, TemplateRef } from '@angular/core';
+import { ModalConfig } from '../interfaces/modal-config';
 
 @Injectable({
   providedIn: 'root'
@@ -11,11 +12,6 @@ export class ModalService {
     console.log('open called');
     return new ModalRef();
   }
-}
-
-export interface ModalConfig{
-  templateRef: TemplateRef<any>;
-  title: string
 }
 
 export class ModalRef{
