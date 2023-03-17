bce42e9305fa10752f029b4e49898bc409445180
Nelson <punkpunkada@gmail.com>
2023-03-17 12:11:46 -0300

    ComponentRef vs ModalRef

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,6 +19,7 @@ export class AppComponent {
    this.modalRef = this.modalService.open({
       templateRef: this.modalTemplateRef,
       title: 'User Details'
-    })
+    });
+    this.modalRef.close();
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ModalModule } from './shared/components/modal/modal.module';
+import { ModalComponent } from './shared/components/modal/modal.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,10 @@ import { ModalModule } from './shared/components/modal/modal.module';
     AppRoutingModule,
     ModalModule
   ],
+  entryComponents:[
+    ModalComponent
+  ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
